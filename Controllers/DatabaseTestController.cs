a38752b275b7d581bf58d2f92a70f46610abbfeb
DarkBear0 <baphometmarx@gmail.com>
2024-11-24 21:29:16 +0100

    db connection test

---
+++ Controllers/DatabaseTestController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DreamApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DatabaseTestController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseTestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    return Ok("бд работает!");
+                }
+            }
+            catch (SqlException ex)
+            {
+                return StatusCode(500, $"Ошибка подключения к базе данных: {ex.Message}");
+            }
+        }
+    }
+}
