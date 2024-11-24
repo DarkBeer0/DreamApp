7c5a5a778c970031db7eafae66b19df6f2a51068
DarkBear0 <baphometmarx@gmail.com>
2024-11-24 16:22:59 +0100

    Test

--- obj/Debug/net6.0/DreamApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/DreamApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DreamApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b75c774bee11b56772a273a50a587c16ef76489")]
 [assembly: System.Reflection.AssemblyProductAttribute("DreamApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DreamApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
